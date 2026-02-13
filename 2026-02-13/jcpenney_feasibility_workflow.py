a3dcb6d88ca3eb612c24e3a298a6cdb5744865a4
Sharon George <georgesharon6969@gmail.com>
2026-02-13 12:17:35 +0530

    Add Playwright import to feasibility workflow

--- 2026-02-13/jcpenney_feasibility_workflow.py
+++ 2026-02-13/jcpenney_feasibility_workflow.py
@@ -1,5 +1,6 @@
 import requests
 from parsel import Selector
+from playwright.sync_api import sync_playwright
 
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
