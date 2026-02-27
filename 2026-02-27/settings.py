101e72e1af05dfdc3ebac8f19ba4f7fe2948eec8
Sharon George <georgesharon6969@gmail.com>
2026-02-27 16:25:49 +0530

    Update MONGO_URI for database connection

--- 2026-02-27/settings.py
+++ 2026-02-27/settings.py
@@ -10,8 +10,8 @@ MONGO_COLLECTION_RESPONSE = f"{PROJECT_NAME}_url"
 MONGO_COLLECTION_DATA = f"{PROJECT_NAME}_data"
 MONGO_COLLECTION_URL_FAILED = f"{PROJECT_NAME}_url_failed"
 
-MONGO_URI = "mongodb://localhost:27017/"
-# MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@209.97.183.63:27017/?authSource=admin"
+# MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@209.97.183.63:27017/?authSource=admin"
 
 # Export settings
 from datetime import datetime
