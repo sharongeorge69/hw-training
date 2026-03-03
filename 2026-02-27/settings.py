cde4980c03b3b5e75abdf5a23ba3226ae4f57c1f
Sharon George <georgesharon6969@gmail.com>
2026-03-03 16:39:39 +0530

    Refactor MongoDB URI and update file paths
    Updated MongoDB URI to use localhost and commented out the previous URI. Adjusted the file path for the full dump CSV file and modified the page size in JSON data structures.

--- 2026-02-27/settings.py
+++ 2026-02-27/settings.py
@@ -10,28 +10,30 @@ MONGO_COLLECTION_RESPONSE = f"{PROJECT_NAME}_url"
 MONGO_COLLECTION_DATA = f"{PROJECT_NAME}_data"
 MONGO_COLLECTION_URL_FAILED = f"{PROJECT_NAME}_url_failed"
 
-# MONGO_URI = "mongodb://localhost:27017/"
-MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@209.97.183.63:27017/?authSource=admin"
+MONGO_URI = "mongodb://localhost:27017/"
+# MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@209.97.183.63:27017/?authSource=admin"
 
 # Export settings
+import os
 from datetime import datetime
-FILE_NAME_FULLDUMP = f"{PROJECT_NAME}_{datetime.now().strftime('%Y_%m_%d')}_{'sample'}.csv"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_NAME_FULLDUMP = os.path.join(BASE_DIR, f"{PROJECT_NAME}_{datetime.now().strftime('%Y_%m_%d')}_{'sample'}.csv")
 EXTRACTION_DATE = datetime.now().strftime('%Y-%m-%d')
 
-cookies = {
-    'nms_mgo_city': 'Mumbai',
-    'nms_mgo_state_code': 'MH',
-    '_fbp': 'fb.1.1772161296788.92828821',
-    'WZRK_G': '01b0a65e76e848b79c0977ecd7b3f458',
-    '_gcl_au': '1.1.1687434808.1772161298',
-    '_ga': 'GA1.1.1329720305.1772161298',
-    'nms_mgo_pincode': '400001',
-    'AKA_A2': 'A',
-    '__tr_luptv': '1772183158773',
-    '_ga_XHR9Q2M3VV': 'GS2.1.s1772182615$o5$g1$t1772183168$j49$l0$h998094909',
-    'WZRK_S_88R-W4Z-495Z': '%7B%22p%22%3A14%2C%22s%22%3A1772182617%2C%22t%22%3A1772183168%7D',
-    'RT': '"z=1&dm=www.jiomart.com&si=46980c59-ace0-4043-9b58-8dfe7e6e591e&ss=mm4nrtt3&sl=3&tt=1n5&obo=2&rl=1"',
-}
+# cookies = {
+#     'nms_mgo_city': 'Mumbai',
+#     'nms_mgo_state_code': 'MH',
+#     '_fbp': 'fb.1.1772161296788.92828821',
+#     'WZRK_G': '01b0a65e76e848b79c0977ecd7b3f458',
+#     '_gcl_au': '1.1.1687434808.1772161298',
+#     '_ga': 'GA1.1.1329720305.1772161298',
+#     'nms_mgo_pincode': '400001',
+#     'AKA_A2': 'A',
+#     '__tr_luptv': '1772183158773',
+#     '_ga_XHR9Q2M3VV': 'GS2.1.s1772182615$o5$g1$t1772183168$j49$l0$h998094909',
+#     'WZRK_S_88R-W4Z-495Z': '%7B%22p%22%3A14%2C%22s%22%3A1772182617%2C%22t%22%3A1772183168%7D',
+#     'RT': '"z=1&dm=www.jiomart.com&si=46980c59-ace0-4043-9b58-8dfe7e6e591e&ss=mm4nrtt3&sl=3&tt=1n5&obo=2&rl=1"',
+# }
 
 headers = {
     'accept': '*/*',
@@ -63,16 +65,94 @@ headers_price_api = {
     'sec-fetch-site': 'same-origin',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
     'x-requested-with': 'XMLHttpRequest',
-    # 'cookie': 'AKA_A2=A; nms_mgo_city=Mumbai; nms_mgo_state_code=MH; _fbp=fb.1.1772161296788.92828821; WZRK_G=01b0a65e76e848b79c0977ecd7b3f458; _gcl_au=1.1.1687434808.1772161298; _ga=GA1.1.1329720305.1772161298; nms_mgo_pincode=400001; __tr_luptv=1772162573309; _ga_XHR9Q2M3VV=GS2.1.s1772161297$o1$g1$t1772162598$j34$l0$h1344873370; WZRK_S_88R-W4Z-495Z=%7B%22p%22%3A41%2C%22s%22%3A1772161297%2C%22t%22%3A1772162598%7D; RT="z=1&dm=www.jiomart.com&si=46980c59-ace0-4043-9b58-8dfe7e6e591e&ss=mm4b2tx2&sl=5&tt=4ff&obo=2&rl=1"',
 }
 
-json_data = {
+json_data_price = {
     'pageCategories': [
         'PDP Variants',
     ],
-    'pageSize': 3,
+    'pageSize': 1,
     'visitorId': 'anonymous-28edee8a-e09b-4d62-8105-77ee466dc366',
     'filter': 'attributes.status:ANY("active")',
     'searchMode': 'PRODUCT_SEARCH_ONLY',
     'branch': 'projects/sr-project-jiomart-jfront-prod/locations/global/catalogs/default_catalog/branches/0',
+}
+
+json_data_crawler = {
+    'pageSize': 50,
+    'facetSpecs': [
+        {
+            'facetKey': {
+                'key': 'brands',
+            },
+            'limit': 500,
+            'excludedFilterKeys': [
+                'brands',
+            ],
+        },
+        {
+            'facetKey': {
+                'key': 'categories',
+            },
+            'limit': 500,
+            'excludedFilterKeys': [
+                'categories',
+            ],
+        },
+        {
+            'facetKey': {
+                'key': 'attributes.category_level_4',
+            },
+            'limit': 500,
+            'excludedFilterKeys': [
+                'attributes.category_level_4',
+            ],
+        },
+        {
+            'facetKey': {
+                'key': 'attributes.category_level_1',
+            },
+            'excludedFilterKeys': [
+                'attributes.category_level_4',
+            ],
+        },
+        {
+            'facetKey': {
+                'key': 'attributes.avg_selling_price',
+                'return_min_max': True,
+                'intervals': [
+                    {
+                        'minimum': 0.1,
+                        'maximum': 100000000,
+                    },
+                ],
+            },
+        },
+        {
+            'facetKey': {
+                'key': 'attributes.avg_discount_pct',
+                'return_min_max': True,
+                'intervals': [
+                    {
+                        'minimum': 0,
+                        'maximum': 99,
+                    },
+                ],
+            },
+        },
+    ],
+    'variantRollupKeys': [
+        'variantId',
+    ],
+    'branch': 'projects/sr-project-jiomart-jfront-prod/locations/global/catalogs/default_catalog/branches/0',
+    'pageCategories': [
+        '29009',
+    ],
+    'userInfo': {
+        'userId': None,
+    },
+    'pageToken': 'U2MlNWZzQmN4IzYz0iZihDOtczY2ITLwADMw0SNkZ2MkRzY2QiGB8v3lrJEG0sqoPMCMIBM1IgC',
+    'orderBy': 'attributes.popularity desc',
+    'filter': 'attributes.status:ANY("active") AND attributes.category_ids:ANY("29009") AND (attributes.available_regions:ANY("TXCF", "PANINDIAGROCERIES")) AND (attributes.inv_stores_1p:ANY("ALL", "T7GZ") OR attributes.inv_stores_3p:ANY("ALL", "groceries_zone_non-essential_services", "general_zone", "groceries_zone_essential_services"))',
+    'visitorId': 'anonymous-9e203381-835d-40cd-8b12-4737626d226b',
 }
