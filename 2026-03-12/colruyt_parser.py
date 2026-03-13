2c718dfe85e69e566703a84ea473b04decc20c6c
Sharon George <georgesharon6969@gmail.com>
2026-03-13 16:25:21 +0530

    Update colruyt_parser.py

--- 2026-03-12/colruyt_parser.py
+++ 2026-03-12/colruyt_parser.py
@@ -41,42 +41,41 @@ class Parser:
         self.product_collection.create_index("unique_id", unique=True)
         logger.info("Connected to MongoDB")
 
-    def fetch_promotion_details(self, promo_id):
-        """Fetch details for a specific promotion ID."""
-        url = "https://apip.colruyt.be/gateway/ictmgmt.emarkecom.promotionretrsvc.v2/v2/nl/promotion"
-        params = {
-            'promotionIds': promo_id,
-            'clientCode': 'CLP',
-            'placeId': '604',
-        }
+    # def fetch_promotion_details(self, promo_id):
+    #     """Fetch details for a specific promotion ID."""
+    #     url = "https://apip.colruyt.be/gateway/ictmgmt.emarkecom.promotionretrsvc.v2/v2/nl/promotion"
+    #     params = {
+    #         'promotionIds': promo_id,
+    #         'clientCode': 'CLP',
+    #         'placeId': '604',
+    #     }
         
-        # Promotion API seems to prefer these exact headers
-        headers = {
-            'accept': 'application/json, text/plain, */*',
-            'accept-language': 'en-US,en;q=0.9,ml;q=0.8',
-            'origin': 'https://www.colruyt.be',
-            'priority': 'u=1, i',
-            'referer': 'https://www.colruyt.be/',
-            'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Linux"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
-            'x-cg-apikey': 'a8ylmv13-b285-4788-9e14-0f79b7ed2411'
-        }
+    #     headers = {
+    #         'accept': 'application/json, text/plain, */*',
+    #         'accept-language': 'en-US,en;q=0.9,ml;q=0.8',
+    #         'origin': 'https://www.colruyt.be',
+    #         'priority': 'u=1, i',
+    #         'referer': 'https://www.colruyt.be/',
+    #         'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+    #         'sec-ch-ua-mobile': '?0',
+    #         'sec-ch-ua-platform': '"Linux"',
+    #         'sec-fetch-dest': 'empty',
+    #         'sec-fetch-mode': 'cors',
+    #         'sec-fetch-site': 'same-site',
+    #         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
+    #         'x-cg-apikey': 'a8ylmv13-b285-4788-9e14-0f79b7ed2411'
+    #     }
         
-        try:
-            time.sleep(random.uniform(1.0, 2.0))
-            resp = self.session.get(url, params=params, headers=headers, impersonate="chrome110", timeout=20)
-            if resp.status_code == 200:
-                return resp.json()
-            else:
-                logger.error(f"Failed [{resp.status_code}] to fetch promotion: {promo_id}")
-        except Exception as e:
-            logger.error(f"Error fetching promotion {promo_id}: {e}")
-        return ""
+    #     try:
+    #         time.sleep(random.uniform(1.0, 2.0))
+    #         resp = self.session.get(url, params=params, headers=headers, impersonate="chrome110", timeout=20)
+    #         if resp.status_code == 200:
+    #             return resp.json()
+    #         else:
+    #             logger.error(f"Failed [{resp.status_code}] to fetch promotion: {promo_id}")
+    #     except Exception as e:
+    #         logger.error(f"Error fetching promotion {promo_id}: {e}")
+    #     return ""
 
     def fetch_product_details(self, tech_art_no):
         """Fetch full product details."""
@@ -108,9 +107,8 @@ class Parser:
 
         for idx, doc in enumerate(self.url_collection.find(), 1):
             technical_article_number = doc.get("technicalArticleNumber")
-            tech_promo_id = doc.get("techPromoId")
+            # tech_promo_id = doc.get("techPromoId")
             commercial_article_number = doc.get("commercialArticleNumber")
-            # Use technicalArticleNumber as our unique primary key for the API payload
             if not technical_article_number:
                 logger.warning(f"Item {idx}/{total} is missing technicalArticleNumber. Skipping.")
                 continue
@@ -127,14 +125,14 @@ class Parser:
             product_details = self.fetch_product_details(technical_article_number)
             
             # 2. Fetch Promotion Details (if applicable)
-            promotion_details = ""
-            if tech_promo_id:
-                promotion_details = self.fetch_promotion_details(tech_promo_id)
+            # promotion_details = ""
+            # if tech_promo_id:
+            #     promotion_details = self.fetch_promotion_details(tech_promo_id)
                 
             # 3. Aggregate data and save
-            self.parse_item(unique_id, doc, product_details, promotion_details)
+            self.parse_item(unique_id, doc, product_details)
 
-    def parse_item(self, unique_id, payload, product_details, promotion_details):
+    def parse_item(self, unique_id, payload, product_details):
         
 
         brand = payload.get('brand', '')
@@ -169,12 +167,19 @@ class Parser:
         producthierarchy = {}
         
         for i, value in enumerate(levels, start=1):
-            producthierarchy[f"producthierarchy_level{i}"] = value                                 
+            producthierarchy[f"producthierarchy_level{i}"] = value   
+
+        price = product_details.get('price')
+        if isinstance(price, dict):
+            basicPrice = price.get('basicPrice')
+            quantityPrice = price.get('quantityPrice') if price.get('quantityPrice') else ""
+            quantityPriceQuantity = price.get('quantityPriceQuantity') if price.get('quantityPriceQuantity') else ""
+        regular_price = str(basicPrice)
+        selling_price = regular_price
 
         # Build breadcrumb string
         breadcrumb = " > ".join(levels) if levels else ""
-        regular_price = str(payload.get('basicPrice', ''))
-        selling_price = regular_price
+        
         promotion_valid_from = str(payload.get('publicationStartDate', ''))
         promotion_valid_upto = str(payload.get('publicationEndDate', ''))
         price_valid_from = promotion_valid_from
@@ -187,7 +192,7 @@ class Parser:
             if d.strip()
         )
         image_url_1 = str(payload.get('fullImage', ''))
-        country_of_origin = str(product_details.get('countryOfOrigin', ''))
+        country_of_origin = str(product_details.get('CountryOfOrigin', ''))
         #allergens
         allergens_data = product_details.get("allergenAttributes", {})
         allergens = []
@@ -199,6 +204,12 @@ class Parser:
                 allergens.append(allergen)
 
         allergens = ", ".join(allergens)
+
+        
+
+        promotion_description = ""
+        if quantityPrice:
+            promotion_description = f"{quantityPrice} vanaf {quantityPriceQuantity} st"
 
         item = {
             "unique_id": unique_id,
@@ -217,33 +228,26 @@ class Parser:
             "promotion_valid_upto": promotion_valid_upto if promotion_valid_upto else "",
             "price_valid_from": price_valid_from if price_valid_from else "",
             "price_per_unit": price_per_unit if price_per_unit else "",
-            "currency": "euro",
+            "currency": "EUR",
             "product_description": description,
             "image_url_1": image_url_1,
             "file_name_1":"",
-            "isAvailable": str(payload.get('isAvailable', '')),
+            "instock": str(payload.get('isAvailable', '')),
             "country_of_origin": country_of_origin,
             "allergens": allergens,
+            "promotion_description": promotion_description,
+            "site_shown_uom": content,
             # Include base crawler payload
-
          
-            "recommendedQuantity": str(payload.get('recommendedQuantity', '')),
-            "quantityPrice": str(payload.get('quantityPrice', '')),
-            "quantityPriceQuantity": str(payload.get('quantityPriceQuantity', '')),
             "measurementUnitQuantityPrice": str(payload.get('measurementUnitQuantityPrice', '')),
             "measurementUnitPrice": str(payload.get('measurementUnitPrice', '')),
             "measurementUnit": str(payload.get('measurementUnit', '')),
             "pricePerUOM": str(payload.get('pricePerUOM', '')),
-            "isAvailable": str(payload.get('isAvailable', '')),
-            "countryOfOrigin": str(payload.get('countryOfOrigin', '')),
             "techPromoId": str(payload.get('techPromoId', '')),
             "promotionId": str(payload.get('promotionId', '')),
-            "promotionType": str(payload.get('promotionType', '')),
-
-            
             # Appended detail JSON
             "product_details": json.dumps(product_details),
-            "promotion_details": json.dumps(promotion_details)
+            # "promotion_details": json.dumps(promotion_details)
         }
         
         # Map hierarchy levels
