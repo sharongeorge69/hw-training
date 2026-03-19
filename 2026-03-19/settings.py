c528f56e79a7380e38e7740d4dbfcf822e0a2a8c
Sharon George <georgesharon6969@gmail.com>
2026-03-19 22:20:59 +0530

    Update settings.py

--- 2026-03-19/settings.py
+++ 2026-03-19/settings.py
@@ -9,8 +9,8 @@ MONGO_COLLECTION_RAW_RESPONSE = f"{PROJECT_NAME}_raw_response"
 MONGO_COLLECTION_URL_FAILED = f"{PROJECT_NAME}_url_failed"
 
 
-# MONGO_URI = "mongodb://127.0.0.1:27017/"
-MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@209.97.183.63:27017/?authSource=admin"
+MONGO_URI = "mongodb://127.0.0.1:27017/"
+# MONGO_URI = "mongodb://mongotraining:a4892e52373844dc4862e6c468d11b6df7938e16@209.97.183.63:27017/?authSource=admin"
 
 
 headers = {
@@ -18,3 +18,10 @@ headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     }
 
+from datetime import datetime
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_NAME = "aldi"
+EXTRACTION_DATE = datetime.now().strftime("%Y-%m-%d")
+FILE_NAME_FULLDUMP = os.path.join(BASE_DIR, f"{PROJECT_NAME}_{datetime.now().strftime('%Y_%m_%d')}_{'sample'}.csv")
+EXPORT_LIMIT = 200
