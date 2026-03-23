bd92dbb9267910cd9c1041b7b212cb0a39f98554
Sharon George <georgesharon6969@gmail.com>
2026-03-23 12:36:20 +0530

    Update aldi_parser.py

--- 2026-03-19/aldi_parser.py
+++ 2026-03-19/aldi_parser.py
@@ -220,16 +220,16 @@ class Parser:
         
         try:
             # Save Raw Response
-            # raw_item = {
-            #     "unique_id": unique_id,
-            #     "html_content": html_content,
-            #     "extraction_date": EXTRACTION_DATE
-            # }
-            # self.raw_collection.update_one(
-            #     {"unique_id": unique_id},
-            #     {"$set": raw_item},
-            #     upsert=True
-            # )
+            raw_item = {
+                "unique_id": unique_id,
+                "html_content": html_content,
+                "extraction_date": EXTRACTION_DATE
+            }
+            self.raw_collection.update_one(
+                {"unique_id": unique_id},
+                {"$set": raw_item},
+                upsert=True
+            )
 
             # Schema validation and save
             product_item = ProductDataItem(**item)
