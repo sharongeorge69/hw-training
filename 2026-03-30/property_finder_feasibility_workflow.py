5a2ea68eb43f5c8b3c33a6ffad9e8f373b0d3dc5
sharon <georgesharon6969@gmail.com>
2026-03-30 13:57:37 +0530

    Added feasibility study of property finder

---
+++ 2026-03-30/property_finder_feasibility_workflow.py
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+############## P A R S E R ####################
+# Parser logic for sales properties
+import requests
+
+headers = {
+    'x-nextjs-data': '1',
+    'sec-ch-ua-platform': '"Linux"',
+    'Referer': 'https://www.propertyfinder.ae/en/transactions/rent/dubai?period=3y&fu=0&rp=y&ob=mr',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+    'sec-ch-ua-mobile': '?0',
+}
+
+params = {
+    'category': 'buy',
+    'slug': 'dubai',
+    'period': '3y',
+    'fu': '0',
+    'ob': 'mr',
+}
+
+response = requests.get(
+    'https://www.propertyfinder.ae/dataguru/_next/data/XGjX_O5nJ3BtMoyVkdeZg/en/transactions/buy/dubai.json',
+    params=params,
+    headers=headers,
+)
+
+data = response.json()
+
+transactions = data['pageProps']['list']['transactionList']
+
+# Parser logic for rental properties
+import requests
+
+headers = {
+    'x-nextjs-data': '1',
+    'sec-ch-ua-platform': '"Linux"',
+    'Referer': 'https://www.propertyfinder.ae/en/transactions/buy/dubai/dubai-land-liwan-wavez-residence?fu=0&ob=mr',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+    'sec-ch-ua-mobile': '?0',
+}
+
+params = {
+    'category': 'rent',
+    'slug': [
+        'dubai',
+        'dubai-land-liwan-wavez-residence',
+    ],
+    'fu': '0',
+    'rp': 'y',
+    'ob': 'mr',
+}
+
+response = requests.get(
+    'https://www.propertyfinder.ae/dataguru/_next/data/XGjX_O5nJ3BtMoyVkdeZg/en/transactions/rent/dubai/dubai-land-liwan-wavez-residence.json',
+    params=params,
+    headers=headers,
+)
+
+data = response.json()
+
+transactions = data['pageProps']['list']['transactionList']
