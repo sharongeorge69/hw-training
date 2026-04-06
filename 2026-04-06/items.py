a78725941a2eec95e535856a28d8920ab8558097
Sharon George <georgesharon6969@gmail.com>
2026-04-06 11:25:28 +0530

    Refactor items.py to include ProductDataItem class
    Added a new ProductDataItem class for storing Moglix product data.

--- 2026-04-06/items.py
+++ 2026-04-06/items.py
@@ -1,5 +1,5 @@
 from mongoengine import DynamicDocument, StringField
-import settings
+from settings import MONGO_COLLECTION_RESPONSE, MONGO_COLLECTION_DATA
 
 class ResponseURLItem(DynamicDocument):
     """
@@ -7,8 +7,24 @@ class ResponseURLItem(DynamicDocument):
     """
     meta = {
         "db_alias": "default",
-        "collection": settings.MONGO_COLLECTION_RESPONSE
+        "collection": MONGO_COLLECTION_RESPONSE
     }
     pdp_url = StringField(unique=True, required=True)
     category_name = StringField()
     category_url = StringField()
+
+class ProductDataItem(DynamicDocument):
+    """
+    Collection for Moglix product data.
+    """
+    meta = {
+        "db_alias": "default",
+        "collection": MONGO_COLLECTION_DATA
+    }
+    product_page_url = StringField(unique=True, required=True)
+    product_name = StringField()
+    product_specifications = StringField()  
+    product_description = StringField()
+    product_features = StringField()
+    product_image_url = StringField()
+    product_video_url = StringField()
