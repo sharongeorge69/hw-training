36678d840f3db9e1a30c892fc845dc451ae36119
Sharon George <georgesharon6969@gmail.com>
2026-05-29 12:24:21 +0530

    Update items.py

--- 2026-05-28/items.py
+++ 2026-05-28/items.py
@@ -22,3 +22,32 @@ class ResponseURLItem(DynamicDocument):
     seller_id = StringField()
     country_of_origin = StringField()
     extraction_date = StringField()
+
+class ProductDataItem(DynamicDocument):
+    meta = {
+        "db_alias": "default",
+        "collection": settings.MONGO_COLLECTION_DATA
+    }
+    product_url = StringField(unique=True, required=True)
+    product_id = StringField()
+    product_name = StringField()
+    brand = StringField()
+    taxonomy = StringField()
+    category_name = StringField()
+    selling_price = StringField()
+    mrp = StringField()
+    discount_percentage = StringField()
+    promotion_description = StringField()
+    product_rating = StringField()
+    product_availability = StringField()
+    stock_count = StringField()
+    product_description = StringField()
+    instructions = StringField()
+    storage_instructions = StringField()
+    main_image_url = StringField()
+    image_urls = ListField(StringField())
+    highlights = StringField()
+    pincode = StringField()
+    store_id = StringField()
+    extraction_datetime = StringField()
+    specification = ListField(DictField())
